3c8a34dd39e5b01a598a23e4745a929689b5c4be
wjdwodnd93 <jjw_0609@naver.com>
2024-11-24 21:14:32 +0900

    react-router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {RouterProvider} from 'react-router-dom';
+import root from './router/root';
+
 
 function App() {
   return (
-    <div className="text-4xl font-extrabold m-2 p-3 bg-green-500">Hello World</div>
+    <RouterProvider router={root} />
   );
 }
 
