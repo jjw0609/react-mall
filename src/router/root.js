66d7b7ae2de7eaea9d7feb1c73845da57926248e
wjdwodnd93 <jjw_0609@naver.com>
2024-11-28 21:00:34 +0900

    Outlet 서브메뉴 적용

--- src/router/root.js
+++ src/router/root.js
@@ -6,6 +6,10 @@ const Main = lazy(() => import('../pages/MainPage'))
 
 const About = lazy(() => import('../pages/AboutPage'));
 
+const TodoIndex = lazy(() => import('../pages/todo/IndexPage'));
+
+const TodoList = lazy(() => import('../pages/todo/ListPage'));
+
 const root = createBrowserRouter([
     {
         path: '',
@@ -14,6 +18,16 @@ const root = createBrowserRouter([
     {
         path: 'about',
         element: <Suspense fallback={Loading}><About /></Suspense>
+    },
+    {
+        path: 'todo',
+        element: <Suspense fallback={Loading}><TodoIndex /></Suspense>,
+        children: [
+            {
+                path: 'list',
+                element: <Suspense fallback={Loading}><TodoList /></Suspense>
+            }
+        ]
     }
 ]);
 
