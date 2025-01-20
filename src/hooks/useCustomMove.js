7c4d2d7a283a29dc65b08d2640705a96740d002b
wjdwodnd93 <jjw_0609@naver.com>
2025-01-20 21:29:01 +0900

    조회 페이지 이동

--- src/hooks/useCustomMove.js
+++ src/hooks/useCustomMove.js
@@ -47,7 +47,11 @@ const useCustomMove = () => {
         navigate({pathname: `../modify/${num}`, search: queryDefault});
     }
 
-    return {moveToList, moveToModify, page, size, refresh}
+    const moveToRead = (num) => {
+        navigate({pathname: `../read/${num}`, search: queryDefault});
+    }
+
+    return {moveToList, moveToModify, moveToRead, page, size, refresh}
 }
 
 
